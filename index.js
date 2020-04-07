110c329bb9d113303267574e2234af7385fbc666
bxchang04 <42091866+bxchang04@users.noreply.github.com>
2020-04-07 10:51:01 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const position = { x: 0, y: 0 }
+
+interact('.draggable').draggable({
+  listeners: {
+    start (event) {
+      console.log(event.type, event.target)
+    },
+    move (event) {
+      position.x += event.dx
+      position.y += event.dy
+
+      event.target.style.transform =
+        `translate(${position.x}px, ${position.y}px)`
+    },
+  }
+})
+
+interact(target).draggable({
+  cursorChecker: (action, interactable, element, interacting) => {
+    switch (action.axis) {
+      case 'x': return 'ew-resize'
+      case 'y': return 'ns-resize'
+      default: return interacting ? 'grabbing' : 'grab'
+    }
+  }
+})
